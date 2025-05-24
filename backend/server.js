6d7735a491cdc13b8a962524b076ff912e817dc1
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2025-05-24 23:19:49 +0200

    chore: finished the database setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
@@ -9,9 +10,20 @@ app.get('/', (req, res) => {
 }
 );
 
-const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT:${PORT}`);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 }
 );
